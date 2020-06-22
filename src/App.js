f624665bb4a2217ef558dc7267b37a39ff2a16f4
Amy Westcott <amy.westcott@yahoo.com>
2020-06-22 15:57:30 -0600

    home page set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="container" align="center">
+        <Switch>
+          <Route exact path="/" component={ Home } />
+         
+        </Switch>
+      </div>
+  </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Habit Tracker!</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
